85efd2ae0be7407b70f63fa549942af7633477fc
He Huang <he.huang.ucandoit@gmail.com>
2016-12-03 17:38:52 -0500

    put all htmls in root dir

--- 13/controller.js
+++ 13/controller.js
@@ -3,10 +3,10 @@ var app = angular.module('mainApp', ['ngRoute']);
 app.config(function($routeProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: '13/home.html'
+		templateUrl: 'home.html'
 	})
 	.when('/anotherPage', {
-		template: '13/anotherPage.html'
+		template: 'anotherPage.html'
 	})
 	.otherwise({
 		redirectTo: '/'
